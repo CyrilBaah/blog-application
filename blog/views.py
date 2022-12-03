15424b9d27a8a7ffc0a7f93a2c4d6a488656e776
CyrilBaah <cyril.baah@amalitech.org>
2022-12-03 06:22:09 +0000

    post detail

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from .models import Post
+from django.http import Http404
 
 
 def post_list(request):
+    """List all posts"""
     posts = Post.published.all()
     return render(request, 'blog/post/list.html', { 'posts': posts })
+
+def post_detial(request, id):
+    try:
+        post = Post.published.get(id=id)
+    except Post.DoesNotExist:
+        raise Http404("No Post found.")
+    
+    return render(request, 'blog/post/detial.html', { 'post': post })
