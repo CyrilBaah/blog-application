0d4fb51ccfc406b49f7c5343b0f1c208ac9df6e8
CyrilBaah <cyril.baah@amalitech.org>
2022-12-04 19:11:27 +0000

    PostList View | classbased

--- blog/views.py
+++ blog/views.py
@@ -1,21 +1,28 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage
+from django.views.generic import ListView
 
 
-def post_list(request):
-    """List all posts"""
-    post_list = Post.published.all()
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
+# def post_list(request):
+#     """List all posts"""
+#     post_list = Post.published.all()
     
-    # Pagination with 3 posts per page
-    paginator = Paginator(post_list, 3)
-    page_number = request.GET.get('page', 1)
-    try:
-        posts = paginator.page(page_number)
-    except EmptyPage:
-        # if page_number is out of range deliver last page of results
-        posts = paginator.page(paginator.num_pages)
-    return render(request, 'blog/post/list.html', { 'posts': posts })
+#     # Pagination with 3 posts per page
+#     paginator = Paginator(post_list, 3)
+#     page_number = request.GET.get('page', 1)
+#     try:
+#         posts = paginator.page(page_number)
+#     except EmptyPage:
+#         # if page_number is out of range deliver last page of results
+#         posts = paginator.page(paginator.num_pages)
+#     return render(request, 'blog/post/list.html', { 'posts': posts })
 
 def post_detail(request, year, month, day, post):
    post = get_object_or_404(Post, 
