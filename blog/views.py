65907f3e7739e54bad93824b43cfd264ea9ff922
CyrilBaah <cyril.baah@amalitech.org>
2022-12-04 18:58:35 +0000

    pagination

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.core.paginator import Paginator, EmptyPage
 
 
 def post_list(request):
     """List all posts"""
-    posts = Post.published.all()
+    post_list = Post.published.all()
+    
+    # Pagination with 3 posts per page
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except EmptyPage:
+        # if page_number is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
     return render(request, 'blog/post/list.html', { 'posts': posts })
 
 def post_detail(request, year, month, day, post):
